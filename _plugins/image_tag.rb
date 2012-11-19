17275c70d1ad4199759c7b8bd2b2275c5140c141
Nate Wiger <nate@wiger.org>
2012-11-19 07:10:38 -0800

    wip: image_tag

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -1,17 +1,21 @@
 # Image tag with optional caption.
 # See: http://www.w3.org/Style/Examples/007/figures.en.html
 class ImageTag < Liquid::Tag
-  def initialize(tag_name, name, tokens)
-    super tag_name, name, tokens
-    @path = '/images'
+  PATH = '/images'
+
+  # This handles parsing all args other than the first one.
+  # So image_tag('myimage.png', :a => 'b', :c => 'd') yields
+  # the string ":a => 'b', :c => 'd'" to this.
+  def parse(markup)
+
+
   end
 
   def render(context)
-    return "ImageTag: #{tag_name.inspect}, #{name.inspect}, #{tokens.inspect}"
-
-    #%Q(<div class="figure"<img src="#{name}"
-        #alt="#{title}"><p>#{caption}</p></div>)
+    %Q(<div class="figure"<img src="#{name}"
+        alt="#{title}"><p>#{caption}</p></div>)
   end
 end
 
 Liquid::Template.register_tag('image_tag', ImageTag)
+
