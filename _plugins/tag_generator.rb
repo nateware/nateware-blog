dd21b991a105d7e150161699d6d53cdef2dca8e7
Nate Wiger <nwiger@gmail.com>
2012-12-01 14:05:53 -0800

    tags and bl link

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -7,7 +7,7 @@ module Jekyll
       @site = site
       @base = base
       @dir  = dir
-      @name = "#{tag}.html"
+      @name = "#{tag.gsub(' ', '-')}.html"
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
