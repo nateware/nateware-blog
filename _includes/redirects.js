fa0d63b1a4ebbe6a6493ee4aa6b03c6313daecaa
Nate Wiger <nwiger@gmail.com>
2013-01-06 14:31:10 -0800

    better 404

--- _includes/redirects.js
+++ _includes/redirects.js
@@ -1,11 +1,11 @@
 /* map of sites */
 $(function(){
-  var req_url = window.location.pathname.substr(1);
+  var req_url = window.location.pathname.substr(1).replace(/\/$/, "");
   var url_map = {
-    "2011/02/10/controlling-what-you-can/" : "controlling-what-you-can.html",
-    "2010/06/14/atomic-rant-redux/"        : "atomic-rant-redux.html",
-    "2010/02/18/an-atomic-rant/"           : "an-atomic-rant.html",
-    "2008/10/16/doppelganger-spotted-in-london/" : "doppelganger-spotted-in-london.html"
+    "2011/02/10/controlling-what-you-can" : "controlling-what-you-can.html",
+    "2010/06/14/atomic-rant-redux"        : "atomic-rant-redux.html",
+    "2010/02/18/an-atomic-rant"           : "an-atomic-rant.html",
+    "2008/10/16/doppelganger-spotted-in-london" : "doppelganger-spotted-in-london.html"
   };
   var new_url = url_map[req_url];
   if (new_url) {
